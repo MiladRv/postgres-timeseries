456e863770d86f3f00d4554bf9c4f8225ed67a68
milad rivandi <m.rivandi@sgity.ir>
2023-10-22 10:40:13 +0330

    feat: configured `Company` in DbContext

--- Postgresql.Infrastructure.Repository/CompanyConfigurationBuilder.cs
+++ Postgresql.Infrastructure.Repository/CompanyConfigurationBuilder.cs
@@ -1,6 +1,15 @@
-﻿namespace Postgresql.Infrastructure.Repository;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Postgresql.Domain;
 
-public class CompanyConfigurationBuilder
+namespace Postgresql.Infrastructure.Repository;
+
+internal sealed class CompanyConfigurationBuilder
+    : IEntityTypeConfiguration<Company>
 {
-    
+    public void Configure(EntityTypeBuilder<Company> builder)
+    {
+        builder
+            .HasKey(p => p.Symbol);
+    }
 }
